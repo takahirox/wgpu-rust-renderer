567f1e83314425f6f768e38558aa9f45629c2c06
Takahiro <hogehoge@gachapin.jp>
2021-10-06 18:15:48 -0700

    Add create_triangle and create_plane methods to GeometryHelper

--- src/utils/geometry_helper.rs
+++ src/utils/geometry_helper.rs
@@ -11,6 +11,95 @@ pub struct GeometryHelper {
 }
 
 impl GeometryHelper {
+	pub fn create_triangle(
+		attribute_manager: &mut AttributeManager,
+		index_manager: &mut IndexManager,
+		width: f32,
+		height: f32,
+	) -> Geometry {
+		let dy = 0.75_f32.sqrt() / 2.0;
+		let positions = [
+			0.0, (0.75_f32.sqrt() - dy) * height, 0.0,
+			0.5 * width, -dy * height, 0.0,
+			-0.5 * width, -dy * height, 0.0,
+		].to_vec();
+
+		let normals = [
+			0.0, 0.0, 1.0,
+			0.0, 0.0, 1.0,
+			0.0, 0.0, 1.0,
+		].to_vec();
+
+		let uvs = [
+			0.5, 0.0,
+			1.0, 1.0,
+			0.0, 1.0,
+		].to_vec();
+
+		let indices = [
+			0, 1, 2,
+		].to_vec();
+
+		let mut geometry = Geometry::new();
+		geometry.set_attribute("position", attribute_manager.create(positions, 3));
+		geometry.set_attribute("normal", attribute_manager.create(normals, 3));
+		geometry.set_attribute("uv", attribute_manager.create(uvs, 2));
+		geometry.set_index(index_manager.create(indices));
+		geometry
+	}
+
+	pub fn create_plane(
+		attribute_manager: &mut AttributeManager,
+		index_manager: &mut IndexManager,
+		width: f32,
+		height: f32,
+	) -> Geometry {
+		let positions = [
+			// top-left
+			-0.5 * width, -0.5 * height, 0.0,
+			// top-right
+			0.5 * width, -0.5 * height, 0.0,
+			// bottom-left
+			-0.5 * width, 0.5 * height, 0.0,
+			// bottom-right
+			0.5 * width, 0.5 * height, 0.0,
+		].to_vec();
+
+		let normals = [
+			// top-left
+			0.0, 0.0, 1.0,
+			// top-right
+			0.0, 0.0, 1.0,
+			// bottom-left
+			0.0, 0.0, 1.0,
+			// bottom-right
+			0.0, 0.0, 1.0,
+		].to_vec();
+
+		let uvs = [
+			// top-left
+			0.0, 0.0,
+			// top-right
+			1.0, 0.0,
+			// bottom-left
+			0.0, 1.0,
+			// bottom-right
+			1.0, 1.0,
+		].to_vec();
+
+		let indices = [
+			0, 1, 2,
+			1, 3, 2,
+		].to_vec();
+
+		let mut geometry = Geometry::new();
+		geometry.set_attribute("position", attribute_manager.create(positions, 3));
+		geometry.set_attribute("normal", attribute_manager.create(normals, 3));
+		geometry.set_attribute("uv", attribute_manager.create(uvs, 2));
+		geometry.set_index(index_manager.create(indices));
+		geometry
+	}
+
 	pub fn create_box(
 		attribute_manager: &mut AttributeManager,
 		index_manager: &mut IndexManager,
@@ -18,8 +107,6 @@ impl GeometryHelper {
 		height: f32,
 		depth: f32,
 	) -> Geometry {
-		let mut geometry = Geometry::new();
-
 		let mut positions = Vec::new();
 		let mut normals = Vec::new();
 		let mut uvs = Vec::new();
@@ -110,11 +197,11 @@ impl GeometryHelper {
 			indices.push(face * 4 + 2);
 		}
 
+		let mut geometry = Geometry::new();
 		geometry.set_attribute("position", attribute_manager.create(positions, 3));
 		geometry.set_attribute("normal", attribute_manager.create(normals, 3));
 		geometry.set_attribute("uv", attribute_manager.create(uvs, 2));
 		geometry.set_index(index_manager.create(indices));
-
 		geometry
 	}
 }
