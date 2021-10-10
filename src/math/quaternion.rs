2b4dd7407eb183516d297fb5e72c0d8db89c96da
Takahiro <hogehoge@gachapin.jp>
2021-10-10 14:39:02 -0700

    Minor clean up

--- src/math/quaternion.rs
+++ src/math/quaternion.rs
@@ -15,13 +15,13 @@ impl Quaternion {
 		let y = e[1];
 		let z = e[2];
 
-		let c1 = ( x / 2.0 ).cos();
-		let c2 = ( y / 2.0 ).cos();
-		let c3 = ( z / 2.0 ).cos();
+		let c1 = (x / 2.0).cos();
+		let c2 = (y / 2.0).cos();
+		let c3 = (z / 2.0).cos();
 
-		let s1 = ( x / 2.0 ).sin();
-		let s2 = ( y / 2.0 ).sin();
-		let s3 = ( z / 2.0 ).sin();
+		let s1 = (x / 2.0).sin();
+		let s2 = (y / 2.0).sin();
+		let s3 = (z / 2.0).sin();
 
 		q[0] = s1 * c2 * c3 + c1 * s2 * s3;
 		q[1] = c1 * s2 * c3 - s1 * c2 * s3;
